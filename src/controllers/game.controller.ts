56c50fb8216968420849a8f35cf70599dc59d3f4
p2204846 <clement.pannecoucke@etu.univ-lyon1.fr>
2024-10-10 16:50:57 +0200

    Exercice 3.2

--- src/controllers/game.controller.ts
+++ src/controllers/game.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Path, Route, Tags } from "tsoa";
+import { Body, Controller, Get, Path, Post, Route, Tags } from "tsoa";
 import { GameDTO } from "../dto/game.dto";
 import { gameService } from "../services/game.service";
 
@@ -15,5 +15,13 @@ export class GameController extends Controller {
     @Path() id : number
   ): Promise<GameDTO | null> {
     return gameService.getGameById(id);
+  }
+
+  @Post("/")
+  public async createGame(
+    @Body() requestBody : GameDTO
+  ): Promise<GameDTO | null> {
+    const { title, console_id } = requestBody;
+    return gameService.createGame(console_id, title)
   }
 }
--- src/services/game.service.ts
+++ src/services/game.service.ts
@@ -29,6 +29,16 @@ export class GameService {
     }
     return game
   }
+
+  public async createGame(
+    console_id: number | undefined,
+    title: string
+  ): Promise<GameDTO | null> {
+    if(console_id){
+      return Game.create({console_id: console_id, title: title});
+    }
+    return notFound("Console")
+  }
 }
 
 export const gameService = new GameService();
