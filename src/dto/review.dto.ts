c409a485fa2dbbfe5de080773f3a130dcc111070
p2204846 <clement.pannecoucke@etu.univ-lyon1.fr>
2024-10-10 17:38:57 +0200

    Exercice 4.1 et 4.2

--- src/dto/review.dto.ts
+++ src/dto/review.dto.ts
@@ -1,4 +1,9 @@
+import { Game } from "../models/game.model";
 import { GameDTO } from "./game.dto";
 
 export interface ReviewDTO {
+    id : number
+    game ?: Game
+    rating ?: number
+    review_text ?: string
 }
--- src/models/review.model.ts
+++ src/models/review.model.ts
@@ -1,17 +1,45 @@
 import { DataTypes, Model } from "sequelize";
 import sequelize from "../config/database"; // Connexion à la base de données
+import { Game } from "./game.model";
+import { allow } from "joi";
 
 export interface ReviewAttributes {
+  id : number;
+  game ?: Game
+  game_id ?: number;
+  rating ?: number;
+  review_text ?: string;
 }
 
-export class Review
-  extends Model<ReviewAttributes>
-  implements ReviewAttributes
+export class Review extends Model<ReviewAttributes> implements ReviewAttributes
 {
+  public id !: number;
+  public game !: Game;
+  game_id !: number;
+  public rating !: number;
+  public review_text !: string;
 }
 
 Review.init(
-  {},
+  {
+    id : {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    game_id : {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    rating : {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    review_text : {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  },
   {
     sequelize,
     tableName: "reviews",
